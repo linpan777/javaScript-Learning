e98807aed4a6ad629006d7cbe21964b6f156090a
linpan777 <linpan115300@gmail.com>
2026-06-24 22:42:21 +0800

    update

---
+++ selfLearning1 - 小費計算器/index.js
@@ -0,0 +1,9 @@
+let food = parseInt(prompt("花多少錢"));
+let tips = food * 0.2;
+
+let total = food + tips;
+
+console.log("小費總額：" + tips + '\n');
+console.log("今日總消費：" + total);
+
+alert("付款完成");
---
+++ test.js
@@ -0,0 +1,12 @@
+let dog =
+{
+    name: "blacky",
+    age: 20,
+    action: console.log("汪汪")    
+}
+dog.action;
+console.log(dog.name);
+dog.color = "black";
+console.log(dog.color);
+
+dog;
